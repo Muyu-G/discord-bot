63f7c604a43c2fc8e3b2d232af4542d8030c0657
Muyu G <179013762+Muyu-G@users.noreply.github.com>
2025-06-27 21:05:13 +0300

    Add some comments

--- cogs/database.py
+++ cogs/database.py
@@ -4,6 +4,7 @@ from datetime import datetime, timezone
 def init_db():
     conn = sqlite3.connect("warnings.db")
     c = conn.cursor()
+    # creating a to store user warnings
     c.execute("""CREATE TABLE IF NOT EXISTS warnings(
               user_id INTEGER,
               guild_id INTEGER,
@@ -13,7 +14,7 @@ def init_db():
         """)
     
     conn.commit()
-    conn.close()
+    conn.close() #closing the DB connection
 
 def add_warning(user_id, guild_id, reason):
     conn = sqlite3.connect("warnings.db")
@@ -34,6 +35,6 @@ def get_warning(user_id, guild_id):
     conn = sqlite3.connect("warnings.db")
     c = conn.cursor()
     c.execute("SELECT reason, timestamp FROM warnings WHERE user_id = ? AND guild_id = ?", (user_id, guild_id))
-    warnings = c.fetchall()
+    warnings = c.fetchall()  #Fetches all rows matching the query
     conn.close()
     return warnings

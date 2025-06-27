d74683c848c2da4f12516a770b9dbaaa8f0e8f58
Muyu G <179013762+Muyu-G@users.noreply.github.com>
2025-06-27 20:55:08 +0300

    Load commands.py from cogs

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
-from database import init_db
+from cogs.database import init_db
 
 init_db()
 
@@ -13,21 +13,24 @@ intents.members = True
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
-bot = commands.Bot(command_prefix = ["g ", "!"], intents = intents)
+bot = commands.Bot(command_prefix = ["!"], intents = intents)
 
 
 @bot.event
 async def on_ready():
     print(f"✅ Logged in as {bot.user}")
     await bot.change_presence(status=discord.Status.dnd, activity=discord.Activity(type=discord.ActivityType.watching, name="Muyu G"))
-
+    
+# syncing extensions
     for guild in bot.guilds:
         await bot.tree.sync(guild=guild)
     print("Slash commands synced!")
-
+    
+# loading extensions
     try:
         await bot.load_extension("cogs.mod_commands")
         await bot.load_extension("cogs.infos")
+        await bot.load_extension("cogs.n_commands")
     except Exception as e:
         print(f"Failed to load cog: {e}")
 
